d698db6c8ded540aa064a28a05b8710877afc97d
Kees Johnson <keesjejohnson@gmail.com>
2023-07-15 20:17:27 +0200

    omgomgomgomgomgomg

--- src/bin/qemu-bios.rs
+++
@@ -1,9 +0,0 @@
-use std::process::{self, Command};
-
-fn main() {
-    let mut qemu = Command::new("qemu-system-x86_64");
-    qemu.arg("-drive");
-    qemu.arg(format!("format=raw,file={}", option_env!("BIOS_IMAGE").unwrap()));
-    let exit_status = qemu.status().unwrap();
-    process::exit(exit_status.code().unwrap_or(-1));
-}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-use std::{env, fs};
+use std::{env, fs, process::{self, Command}};
 
 fn main() {
-    let current_exe = env::current_exe().unwrap();
-    let uefi_target = current_exe.with_file_name("uefi.img");
-    let bios_target = current_exe.with_file_name("bios.img");
-
-    fs::copy(option_env!("UEFI_IMAGE").unwrap(), &uefi_target).unwrap();
-    fs::copy(option_env!("BIOS_IMAGE").unwrap(), &bios_target).unwrap();
-
-    println!("UEFI disk image at {}", uefi_target.display());
-    println!("BIOS disk image at {}", bios_target.display());
+    let mut qemu = Command::new("qemu-system-x86_64");
+    qemu.arg("-drive");
+    qemu.arg(format!("format=raw,file={}", env!("BIOS_IMAGE")));
+    let exit_status = qemu.status().unwrap();
+    process::exit(exit_status.code().unwrap_or(-1));
 }
